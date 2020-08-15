9b69deb0d46663d488d18ae653c966c49343ac40
linbiwei <714054344@qq.com>
2020-08-15 12:11:25 +0800

    .

--- Vue_cli/vue3_demo_luyou/src/App.vue
+++ Vue_cli/vue3_demo_luyou/src/App.vue
@@ -1,29 +1,28 @@
 <template>
   <div id="app">
     <!-- 1. router-link  路由跳转 -->
-      <!-- 
+    <!-- 
         类似 a 标签
         tag 换掉默认渲染的标签 
         replace 取消返回键功能 
         active-class  更改默认选中class 
-       -->
-      <router-link to="/home" tag="button" replace active-class="active">主页</router-link>
-      <router-link to="/about" replace >关于</router-link>
-      <router-link :to="'/user/'+userid" replace >用户中心</router-link>
-      <!-- <router-link to='/profile'>档案</router-link> -->
+    -->
+    <router-link to="/home" tag="button" replace active-class="active">主页</router-link>
+    <router-link to="/about" replace>关于</router-link>
+    <router-link :to="'/user/'+userid" replace>用户中心</router-link>
+    <!-- <router-link to='/profile'>档案</router-link> -->
 
-      <!--  路由传参  query -->
-      <router-link :to="{poth:'/profile',query:{name:'linbiwei'}}">档案</router-link>
+    <!--  路由传参  query -->
+    <!-- <router-link :to="{path:'/profile',query:{name:'linbiwei'}}">档案</router-link> -->
+    <!--  -->
+    <button @click=" profileclick">档案</button>
 
+    <!-- 2. 通过点击代码跳转路由  -->
+    <!-- <button @click="syclick"> 首页</button> -->
+    <!-- <button  @click="gyclick"> 关于</button> -->
 
-
-      <!-- 2. 通过点击代码跳转路由  -->
-      <!-- <button @click="syclick"> 首页</button> -->
-      <!-- <button  @click="gyclick"> 关于</button> -->
-
-  
-      <!-- 显示位置 站位 -->
-      <router-view></router-view>
+    <!-- 显示位置 站位 -->
+    <router-view></router-view>
   </div>
 </template>
 
@@ -44,6 +43,14 @@
       gyclick(){
         // this.$router.path('/about')
         this.$router.replace('/about')
+      },
+      profileclick(){
+         this.$router.pust({
+           path:'/profile',
+           query:{
+             name:'linbiwei'
+            }
+         })
       }
     }
 
@@ -51,5 +58,7 @@
 </script>
 
 <style >
-  .active{color: brown;}
+.active {
+  color: brown;
+}
 </style>
